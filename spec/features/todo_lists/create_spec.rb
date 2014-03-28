82154a58aec5755e1e5b304c6c8274872c3f4146
dimdimsum <orabondo@gmail.com>
2014-03-28 10:30:44 -0400

    Add validations for description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,17 +1,40 @@
 require 'spec_helper'
 
 describe "Creating to do lists" do 
-	it "redirects to the todo_list index page on success" do
+	def create_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list."
+
 		visit "/todo_lists"
 		click_link "New Todo list"
 		expect(page).to have_content("New todo_list")
 
-		fill_in "Title", with:"My Todo List"
-		fill_in "Description", with:"This is what i'm doing today"
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
 
 		expect(page).to have_content("My Todo List")
 	end
+
+
+	it "redirects to the todo_list index page on success" do
+		create_todo_list
+		expect(page).to have_content("My Todo List")
+	end
+
+	it "displays error when the todo list has no title" do 
+		expect(TodoList.count).to eq(0)
+
+		create_todo_list title: ""
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what i'm doing today")
+
+	end
+
 
 	it "displays an error when the todo list has a title less tha 3 characters" do
 		expect(TodoList.count).to eq(0)
